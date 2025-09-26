d8ae4e6d18191e53a1922db40610038513884bb8
Samuel da Silva B Jr <12252200+SamuelSBJr97@users.noreply.github.com>
2025-09-26 07:44:31 -0300

    update

--- src/game-test/test_project.py
+++
@@ -1,9 +0,0 @@
-from game.project import Depurador
-from game.game import Game
-
-
-def test_project_builds_and_attaches():
-    proj = Depurador("TestCity")
-    g = Game()
-    proj.attach_to_game(g)
-    assert g.running
