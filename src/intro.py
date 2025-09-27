2859c93f659d0a1f78f2f4eebb31a57bce1ce238
Samuel da Silva B Jr <12252200+SamuelSBJr97@users.noreply.github.com>
2025-09-27 07:08:19 -0300

    update

--- src/intro.py
+++ src/intro.py
@@ -16,9 +16,12 @@ class BattleBugs:
         self.bullets = []
         self.objects = []
         self.player: Actor
+        self.state = 'intro'  # intro, start, gameover
 
-    def start(self):
-        self.board = []
+    def intro(self, screen):
+        if len(self.board) > 0:
+            return
+        
         # algoritmo para gerar chão que o player e bugs podem andar
         for x in range(16):
             for y in range(10):
@@ -29,6 +32,11 @@ class BattleBugs:
                     floor.y = y * 50 + 25 + 50
                     self.board.append(floor)
 
+        self.objects = [*self.board]
+
+        screen.draw.text(f'Bugs restantes: {len(self.bugs)}', (10, 10), color='white')
+
+    def start(self):
         self.boardsSelecteds = []
 
         self.bugs = []
@@ -54,8 +62,11 @@ class BattleBugs:
         self.player.x = _board.x
         self.player.y = _board.y
 
+        self.objects = []
         # passa referencia dos objetos para facilitar o draw/update
         self.objects = [*self.board, *self.bugs, self.player]
+
+        self.state = 'game'
 
     def next_board(self, actor, forward=True, bullet=False):
         x = actor.x
@@ -113,90 +124,111 @@ class BattleBugs:
         actor.image = actor.images[random.randint(0, len(actor.images) - 1)]
         actor.angle = angle
 
+    def key_down(self, keyboard):
+        if self.state != 'game':
+            return
+
+        if keyboard.Escape:
+            self.reset()
+            return
+
+        if keyboard.left or keyboard.right or keyboard.up or keyboard.down:
+            angle = self.player.angle
+
+            # right or left fica girando
+            if keyboard.right or keyboard.left:
+                if keyboard.right:
+                    angle -= 90
+                elif keyboard.left:
+                    angle += 90
+                
+                angle = angle % 360
+
+                self.player.angle = angle
+
+            elif keyboard.up or keyboard.down:
+                self.next_board(self.player, keyboard.up == True)
+
+        elif keyboard.space:
+            bullet = Actor('bullet-1', anchor=('center', 'center'))
+            bullet.images = ['bullet-1', 'bullet-2', 'bullet-3']
+            bullet.angle = self.player.angle
+            bullet.x = self.player.x
+            bullet.y = self.player.y
+            self.objects.append(bullet)
+            self.bullets.append(bullet)
+
+        if len(self.bugs) > 0 and (keyboard.left or keyboard.right or keyboard.up or keyboard.down or keyboard.space):
+            bug = random.choice(self.bugs)
+
+            # Calcula a diferença de posição entre o bug e o player
+            dx = abs(self.player.x - bug.x)
+            dy = abs(self.player.y - bug.y)
+
+            # Decide a direção prioritária para o bug se mover em direção ao player
+            if dx > dy:
+                # Move horizontalmente
+                if dx > 0:
+                    angle = 270  # direita
+                else:
+                    angle = 90   # esquerda
+            else:
+                # Move verticalmente
+                if dy > 0:
+                    angle = 180  # baixo
+                else:
+                    angle = 0    # cima
+
+            bug.angle = angle
+            self.next_board(bug, dx < dy)
+
+    def update(self, dt, keyboard):
+        if self.state != 'game':
+            return
+
+        for bullet in self.bullets:
+            hasHit = False
+            for bug in self.bugs:
+                if bug.x == bullet.x and bug.y == bullet.y:
+                    hasHit = True
+                    self.objects.remove(bug)
+                    self.bugs.remove(bug)
+
+                    for _board in self.boardsSelecteds:
+                        if bug.x == _board.x and bug.y == _board.y:
+                            self.boardsSelecteds.remove(_board)
+                            break
+                    break
+
+            if hasHit or not self.next_board(bullet, True, True):
+                self.objects.remove(bullet)
+                self.bullets.remove(bullet)
+
+    def draw(self, screen, keyboard):
+        screen.clear()
+
+        for obj in self.objects:
+            self.actor_anime(obj)
+            obj.draw()
+
+        if self.state == 'intro':
+            self.intro(screen)
+            screen.draw.text('Pressione Enter para iniciar', center=(400, 300), color='white', fontsize=40)
+            if keyboard.RETURN:
+                self.state = 'start'
+        elif self.state == 'start':
+            self.start()
+
 # Inicializa o jogo
 battleBugs = BattleBugs()
 
 def on_key_down(key):
-    if keyboard.Escape:
-        battleBugs.reset()
-        return
-
-    if keyboard.left or keyboard.right or keyboard.up or keyboard.down:
-        angle = battleBugs.player.angle
-
-        # right or left fica girando
-        if keyboard.right or keyboard.left:
-            if keyboard.right:
-                angle -= 90
-            elif keyboard.left:
-                angle += 90
-            
-            angle = angle % 360
-
-            battleBugs.player.angle = angle
-
-        elif keyboard.up or keyboard.down:
-            battleBugs.next_board(battleBugs.player, keyboard.up == True)
-
-    elif keyboard.space:
-        bullet = Actor('bullet-1', anchor=('center', 'center'))
-        bullet.images = ['bullet-1', 'bullet-2', 'bullet-3']
-        bullet.angle = battleBugs.player.angle
-        bullet.x = battleBugs.player.x
-        bullet.y = battleBugs.player.y
-        battleBugs.objects.append(bullet)
-        battleBugs.bullets.append(bullet)
-
-    if len(battleBugs.bugs) > 0 and (keyboard.left or keyboard.right or keyboard.up or keyboard.down or keyboard.space):
-        bug = random.choice(battleBugs.bugs)
-
-        # Calcula a diferença de posição entre o bug e o player
-        dx = abs(battleBugs.player.x - bug.x)
-        dy = abs(battleBugs.player.y - bug.y)
-
-        # Decide a direção prioritária para o bug se mover em direção ao player
-        if dx > dy:
-            # Move horizontalmente
-            if dx > 0:
-                angle = 270  # direita
-            else:
-                angle = 90   # esquerda
-        else:
-            # Move verticalmente
-            if dy > 0:
-                angle = 180  # baixo
-            else:
-                angle = 0    # cima
-
-        bug.angle = angle
-        battleBugs.next_board(bug, dx < dy)
+    battleBugs.key_down(keyboard)
 
 def update(dt):
-    for bullet in battleBugs.bullets:
-        hasHit = False
-        for bug in battleBugs.bugs:
-            if bug.x == bullet.x and bug.y == bullet.y:
-                hasHit = True
-                battleBugs.objects.remove(bug)
-                battleBugs.bugs.remove(bug)
-
-                for _board in battleBugs.boardsSelecteds:
-                    if bug.x == _board.x and bug.y == _board.y:
-                        battleBugs.boardsSelecteds.remove(_board)
-                        break
-                break
-
-        if hasHit or not battleBugs.next_board(bullet, True, True):
-            battleBugs.objects.remove(bullet)
-            battleBugs.bullets.remove(bullet)
+    battleBugs.update(dt, keyboard)
 
 def draw():
-    screen.clear()
-
-    for obj in battleBugs.objects:
-        battleBugs.actor_anime(obj)
-        obj.draw()
-
-    screen.draw.text(f'Bugs restantes: {len(battleBugs.bugs)}', (10, 10), color='white')
+    battleBugs.draw(screen, keyboard)
 
 pgzrun.go()
